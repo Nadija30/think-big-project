9cdd2d0c9370c6b01d738566be7508de5d52c580
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-08-18 17:24:58 +0300

    commit

--- src/js/search-api.js
+++ src/js/search-api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
 const END_POINTS = {
+    events: '/events',
     recipes: '/recipes',
     areas: '/areas',
     categories: '/categories',
@@ -10,6 +11,11 @@ const END_POINTS = {
 };
 
 axios.defaults.baseURL = BASE_URL;
+
+export const getEvents = async () => {
+    const response = await axios(`${END_POINTS.events}`);
+    return response.data;
+};
 
 export const getAreas = async () => {
     const response = await axios(`${END_POINTS.areas}`);
