343d3a25af7bc8b693a1555336ef3a82afbca21d
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-08-18 17:02:50 +0300

    1.1

--- src/js/order.modal.js
+++ src/js/order.modal.js
@@ -1,15 +1,20 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
   btnOpen: document.querySelector('[data-order-btn-open]'),
   btnClose: document.querySelector('[data-order-btn-close]'),
   backdrop: document.querySelector('[data-order-backdrop]'),
   scrollOnModal: document.querySelector('body'),
+  btnSend: document.querySelector('.order-form-btn'),
 };
 
 refs.btnOpen.addEventListener('click', onBtnOpenClick);
 refs.btnClose.addEventListener('click', onBtnCloseClick);
 refs.backdrop.addEventListener('click', onBackdropClick);
 
-function onBtnOpenClick() {
+refs.btnSend.addEventListener('submit', onBtnSendClick);
+
+function onBtnOpenClick(event) {
   window.addEventListener('keydown', onEscPress);
   refs.scrollOnModal.classList.add('scroll-blocked');
   refs.backdrop.classList.remove('is-hidden');
@@ -33,4 +38,9 @@ function onEscPress(event) {
   if (event.code === ESC_KEY_CODE) {
     onBtnCloseClick();
   }
+}
+
+function onBtnSendClick(event) {
+  event.preventDefault();
+  console.log(event);
 }
