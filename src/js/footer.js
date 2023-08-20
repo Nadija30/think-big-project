607eb1c44b5f48598e91ab60c9c1520ff3572fb1
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-08-20 16:26:51 +0200

    footer-modal

--- src/js/footer.js
+++ src/js/footer.js
@@ -12,25 +12,99 @@ function scrollToTop() {
       window.scrollTo({
         top: 0,
         behavior: 'smooth',
-    });
+      });
+  btnUp.blur();
 }
 
+  
 //* =============================================== FEEDBACK MODAL */
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-feedback-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-feedback-modal-close]'),
+//     modal: document.querySelector('[data-feedback-modal]'),
+//   };
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.modal.addEventListener('click', (click) => {
+//     if (click.target === click.currentTarget) toggleModal();
+//   });
+    
+//     document.addEventListener('keydown', (evt) => {
+//     if (evt.key === 'Escape' || evt.key === 'Esc') {
+//       if (!refs.modal.classList.contains('is-hidden')) {
+//         toggleModal();
+//         refs.openModalBtn.blur();
+//       }
+//     }
+      
+//   });
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     document.body.classList.toggle('no-scroll');
+//   }
+// })();
+
+//* =============================================== */
 
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-feedback-modal-open]'),
     closeModalBtn: document.querySelector('[data-feedback-modal-close]'),
     modal: document.querySelector('[data-feedback-modal]'),
+    form: document.querySelector(".feedback-form"),
   };
+
+  const LOCAL_KEY = "feedback-data";
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.modal.addEventListener('click', (click) => {
     if (click.target === click.currentTarget) toggleModal();
   });
-    
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      if (!refs.modal.classList.contains('is-hidden')) {
+        toggleModal();
+        refs.openModalBtn.blur();
+      }
+    }
+  });
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
+
+  refs.form.addEventListener("input", onFormInput);
+  refs.form.addEventListener("submit", onFormSubmit);
+
+  let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+  setFormData();
+
+  function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+  }
+
+  function setFormData() {
+    const savedFormData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    if (savedFormData) {
+      refs.form.user_name.value = savedFormData.user_name || "";
+      refs.form.user_message.value = savedFormData.user_message || "";
+    }
+  }
+
+  function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    localStorage.removeItem(LOCAL_KEY);
+    evt.currentTarget.reset();
+    toggleModal();
+    formData = {};
+  }
 })();
+
