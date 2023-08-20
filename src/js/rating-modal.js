209d839b66d57a33ee68d4ceefd0b5ba26cbc10a
SaltyUA <00cmb.ua@gmail.com>
2023-08-20 20:41:09 +0300

    render star rating

--- src/js/rating-modal.js
+++ src/js/rating-modal.js
@@ -31,9 +31,7 @@ function openRatingModal() {
     refs.ratingModal.classList.remove(`is-hidden`);
 }
 
-initRatings();
-
-function initRatings() {
+export function initRatings() {
     const ratings = document.querySelectorAll(`.rating`);
     let ratingValue, ratingStars;
 
@@ -42,6 +40,7 @@ function initRatings() {
     });
 
     function initRating(rating) {
+        renderIcons(rating);
         initRatingValues(rating);
         setActiveStars();
         if (rating.classList.contains(`set__rating`)) {
@@ -54,7 +53,7 @@ function initRatings() {
         ratingStars = rating.querySelectorAll(`.rating__icon`);
     }
 
-    function setActiveStars(count = ratingValue.innerHTML) {
+    function setActiveStars(count = Math.floor(ratingValue.innerHTML)) {
         ratingStars.forEach(star => {
             star.classList.remove(`active__star`);
         });
@@ -67,6 +66,38 @@ function initRatings() {
     function setNewValue(e) {
         ratingValue.innerHTML = `${e.target.value}.0`;
         setActiveStars();
+    }
+
+    function renderIcons(rating) {
+        const starWrap = document.createElement(`div`);
+
+        for (let i = 0; i < 5; i++) {
+            const starSvg = document.createElementNS(
+                'http://www.w3.org/2000/svg',
+                'svg'
+            );
+            const starPath = document.createElementNS(
+                'http://www.w3.org/2000/svg',
+                'path'
+            );
+
+            starWrap.classList.add(`card__star__wrap`);
+
+            starSvg.setAttribute('viewBox', '0 0 32 32');
+            starSvg.setAttribute('width', '18');
+            starSvg.setAttribute('height', '18');
+
+            starSvg.classList.add('rating__icon');
+
+            starPath.setAttribute(
+                'd',
+                'M13.826 3.262c0.684-2.106 3.663-2.106 4.348 0l1.932 5.945c0.306 0.942 1.184 1.579 2.174 1.579h6.251c2.214 0 3.135 2.833 1.344 4.135l-5.057 3.674c-0.801 0.582-1.136 1.614-0.83 2.556l1.931 5.945c0.684 2.106-1.726 3.857-3.517 2.555l-5.057-3.674c-0.801-0.582-1.886-0.582-2.687 0l-5.057 3.674c-1.791 1.302-4.202-0.45-3.517-2.555l1.932-5.945c0.306-0.942-0.029-1.973-0.83-2.556l-5.057-3.674c-1.791-1.302-0.871-4.135 1.344-4.135h6.251c0.99 0 1.868-0.638 2.174-1.579l1.932-5.945z'
+            );
+
+            starSvg.appendChild(starPath);
+            starWrap.append(starSvg);
+        }
+        rating.append(starWrap);
     }
 }
 
