0397d3c40a3b23fb3640196f00d32293e4ab8b29
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-08-20 18:21:16 +0200

    footer-modal

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,3 +1,18 @@
+import Notiflix from 'notiflix';
+
+  // const notifyInit = Notify.init({
+  // width: '280px',
+  // position: 'right-bottom',
+  // distance: '20px',
+  // // timeout: 2600,
+  // opacity: 0.8,
+  // fontSize: '20px',
+  // borderRadius: '50px 10px',
+  // notiflixIconColor: 'rgba(0,0,0,0.6)',
+  // pauseOnHover: true,
+  // });
+  // Notify.info("Please, tell us what you are looking for.", notifyInit); 
+
 //* =============================================== BTN-UP */
 
 const btnUp = document.querySelector(".btn-up");
@@ -18,35 +33,6 @@ function scrollToTop() {
 
   
 //* =============================================== FEEDBACK MODAL */
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-feedback-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-feedback-modal-close]'),
-//     modal: document.querySelector('[data-feedback-modal]'),
-//   };
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-//   refs.modal.addEventListener('click', (click) => {
-//     if (click.target === click.currentTarget) toggleModal();
-//   });
-    
-//     document.addEventListener('keydown', (evt) => {
-//     if (evt.key === 'Escape' || evt.key === 'Esc') {
-//       if (!refs.modal.classList.contains('is-hidden')) {
-//         toggleModal();
-//         refs.openModalBtn.blur();
-//       }
-//     }
-      
-//   });
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//     document.body.classList.toggle('no-scroll');
-//   }
-// })();
-
-//* =============================================== */
 
 (() => {
   const refs = {
@@ -97,14 +83,14 @@ function scrollToTop() {
       refs.form.user_message.value = savedFormData.user_message || "";
     }
   }
-
+  
   function onFormSubmit(evt) {
     evt.preventDefault();
-
     localStorage.removeItem(LOCAL_KEY);
     evt.currentTarget.reset();
     toggleModal();
     formData = {};
-  }
+     }
 })();
+
 
