30e39809b62f757cb1aba17276b95a11bf2a4434
Святослав Минчаков <sminchakov@gmail.com>
2023-08-21 23:52:55 +0300

    added function to render

--- src/js/favorites-recipes.js
+++ src/js/favorites-recipes.js
@@ -5,37 +5,31 @@ import { getCardByID } from "./search-api"
 
 const favorCatBox = document.querySelector('.favorites__categories-list');
 const favorGallBox = document.querySelector('.favorites__gallery-list');
-const cardsFromMainBox = document.querySelector('.js-cards')
-
-// cardsFromMainBox.addEventListener('click', addToLocalStorage);
-// console.log(cardsFromMainBox)
-
-
 
 
 function getFromLocalStorage() {
     
     for (let key in localStorage) {
-    if (localStorage.hasOwnProperty(key)) {
         const value = localStorage.getItem(key);
-
+        const parsedValue = JSON.parse(value);
+    if (typeof parsedValue === 'object' && parsedValue !== null && 'preview' in parsedValue) {
+        
         try {
-            const parsedValue = JSON.parse(value);
-            console.log(`Key: ${key}, Value: `, parsedValue);
-            console.log(parsedValue.preview)
-        //     const favoriteCard = `<li class="favorites__gallery-list-item">
-        //   <div class="favorites__card">
-        //     <img src="${parsedValue.preview}" alt="${key}" class="favorites__card-image">
-        //     <div class="favorites__card-info">
-        //       <h2 class="favorites__card-tittle">${parsedValue.title}</h2>
-        //       <p class="favorites__card-description">${parsedValue.description}</p>
-        //       <button class="favorites__card-btn">card test</button>
-        //     </div>
-        //     <div class="favorites__card-heart"></div>
-        //   </div>
-        // </li>`;
+            const notatendetBlock = document.querySelector('.favorites__not-atendent');
+            notatendetBlock.classList.add('is-hidden');
+            const favoriteCard = `<li class="favorites__gallery-list-item">
+          <div class="favorites__card">
+            <img src="${parsedValue.preview}" alt="${key}" class="favorites__card-image">
+            <div class="favorites__card-info">
+              <h2 class="favorites__card-tittle">${parsedValue.title}</h2>
+              <p class="favorites__card-description">${parsedValue.description}</p>
+              <button class="favorites__card-btn">card test</button>
+            </div>
+            <div class="favorites__card-heart"></div>
+          </div>
+        </li>`;
             
-            // favorGallBox.insertAdjacentHTML('beforeend', favoriteCard)
+            favorGallBox.insertAdjacentHTML('beforeend', favoriteCard)
         } catch (error) {
             console.error(`Key: ${key}, Error parsing value: `, error);
         }
@@ -43,7 +37,23 @@ function getFromLocalStorage() {
 }
 }
 
+function getCategoriesFromLS() {
+  for (let key in localStorage) {
+    const value = localStorage.getItem(key);
+    const parsedValue = JSON.parse(value);
+
+    if (typeof parsedValue === 'object' && parsedValue !== null && 'preview' in parsedValue) {
+      try {
+        console.log(parsedValue.category)
+      } catch {
+        console.error(`Key: ${key}, Error parsing value: `, error);
+      }
+    }
+  }
+}
+
 getFromLocalStorage()
+getCategoriesFromLS()
 
 
 
