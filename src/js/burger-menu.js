734d9d2d96642164c20568d618c8013ed27fa36e
Tatiana <tatka_co@ukr.net>
2023-08-22 01:08:41 +0300

    header-new

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,11 +1,11 @@
-const mobMenuOpenBtn = document.querySelector('.menu-open-btn');
-const mobMenuCloseBtn = document.querySelector('.mob-menu-btn-close');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobMenuOpenBtn = document.querySelector('.mob-menu-btn-open');
+const mobMenuCloseBtn = document.querySelector('.mob-menu-btn-close');
 
 mobMenuOpenBtn.addEventListener('click', function() {
-  mobileMenu.classList.add('open');
+    mobileMenu.classList.remove('is-hidden');
 });
 
 mobMenuCloseBtn.addEventListener('click', function() {
-  mobileMenu.classList.remove('open');
+    mobileMenu.classList.add('is-hidden');
 });
