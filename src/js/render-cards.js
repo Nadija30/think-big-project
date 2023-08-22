5cf1ac2fa9ba430209058cb7303bc4a6fe39971d
OlehKhv <hvostik.oleg@ukr.net>
2023-08-22 22:22:59 +0300

    fix icon heart, add delete from local storage

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -8,10 +8,11 @@ export function createCards(cards, container) {
         // return;
 
         const notAttendent = document.querySelector('.favorites__not-atendent');
-        const favorCatBox = document.querySelector('.favorites__categories-list');
+        const favorCatBox = document.querySelector(
+            '.favorites__categories-list'
+        );
         notAttendent.classList.remove('is-hidden');
         favorCatBox.classList.add('is-hidden');
-        
     }
 
     container.innerHTML = cards
@@ -29,7 +30,7 @@ export function createCards(cards, container) {
         ),
         url(${preview});
     background-repeat: no-repeat;
-    background-size: cover;"><button class="heart js-btn-heart" data-id="${_id}"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" class="icon-heart">
+    background-size: cover;"><button class="heart js-btn-heart" data-id="${_id}"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" class="icon-heart" data-id="${_id}">
     <path class="heart-svg-path" data-id="${_id}" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.994 4.708C9.162 2.565 6.105 1.988 3.81 3.95c-2.296 1.962-2.62 5.242-.816 7.563 1.5 1.929 6.037 5.998 7.524 7.315.166.147.25.221.346.25a.464.464 0 0 0 .262 0c.097-.029.18-.103.347-.25 1.487-1.317 6.025-5.386 7.524-7.315 1.803-2.32 1.52-5.622-.816-7.563-2.336-1.942-5.353-1.386-7.186.757Z" clip-rule="evenodd"/>
     </svg></button><h3 class="title-card-rec">${title}</h3>
     <p class="text-card-rec"><span class="text-wrap">${description}</span></p>
@@ -39,16 +40,17 @@ export function createCards(cards, container) {
         )
         .join('');
 
-    // getFromLocalStorage();
-    // console.log(getFromLocalStorage());
-    // if(){}
-
-    // document.querySelectorAll('.icon-heart').forEach(icon => {
-    //     icon.classList.add('icon-heart-active');
-    // });
-
-    // document.querySelectorAll('.icon-heart').forEach(icon => {
-    //     icon.classList.toggle('icon-heart-active');
-    // });
     initRatings();
+
+    document.querySelectorAll('.icon-heart').forEach(icon => {
+        if (icon) {
+            getFromLocalStorage()
+                .map(favorite => favorite._id)
+                .forEach(favId => {
+                    if (favId === icon.dataset.id) {
+                        icon.classList.add('icon-heart-active');
+                    }
+                });
+        }
+    });
 }
