5be70e21e0619ff631fb7ba72b3aab43341079bb
OlehKhv <hvostik.oleg@ukr.net>
2023-08-23 14:54:21 +0300

    fix render card

--- src/js/favorites-recipes.js
+++ src/js/favorites-recipes.js
@@ -1,22 +1,27 @@
-
 const favorCatBox = document.querySelector('.favorites__categories-list');
 const favorGallBox = document.querySelector('.favorites__gallery-list');
 
-getFromLocalStorage()
+getFromLocalStorage();
 
 function getFromLocalStorage() {
-    
     for (let key in localStorage) {
         const value = localStorage.getItem(key);
         const parsedValue = JSON.parse(value);
-    if (typeof parsedValue === 'object' && parsedValue !== null && 'preview' in parsedValue) {
-        
-        try {
-          const notAtended = document.querySelector('.favorites__not-atendent');
-          const catList = document.querySelector('.favorites__categories-list');
-          catList.classList.remove('is-hidden');
-          notAtended.classList.add('is-hidden');
-            const favoriteCard = `<li class="favorites__gallery-list-item">
+        if (
+            typeof parsedValue === 'object' &&
+            parsedValue !== null &&
+            'preview' in parsedValue
+        ) {
+            try {
+                const notAtended = document.querySelector(
+                    '.favorites__not-atendent'
+                );
+                const catList = document.querySelector(
+                    '.favorites__categories-list'
+                );
+                catList.classList.remove('is-hidden');
+                notAtended.classList.add('is-hidden');
+                const favoriteCard = `<li class="favorites__gallery-list-item">
           <div class="favorites__card">
             <img src="${parsedValue.preview}" alt="${parsedValue.title}" class="favorites__card-image">
             <div class="favorites__card-info">
@@ -29,65 +34,30 @@ function getFromLocalStorage() {
             </svg></div>
           </div>
         </li>`;
-          
-          const category = `<li class="favorites__categories-item">
+
+                const category = `<li class="favorites__categories-item">
           <button type="button" class="favorites__categories-btn">${parsedValue.category}</button>
           </li>`;
-            
-          favorGallBox.insertAdjacentHTML('beforeend', favoriteCard);
-          favorCatBox.insertAdjacentHTML('beforeend', category);
-        } catch (error) {
-            console.log(`Key: ${key}, Error parsing value: `, error);
+
+                favorGallBox.insertAdjacentHTML('beforeend', favoriteCard);
+                favorCatBox.insertAdjacentHTML('beforeend', category);
+            } catch (error) {
+                console.log(`Key: ${key}, Error parsing value: `, error);
+            }
         }
     }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const categoriesArray = [];
 
-
-
 // getCategories().then(response => {
-    
+
 //     response.map(item => {
 //         categoriesArray.push(`<button class="favorites__categories-btn" type="button">${item.name}</button>`);
 //   });
-    
+
 // });
 
 // const favorCat = categoriesArray.join('')
 
 // console.log(favorCat);
-
-
-
-
-
-
-
-
-
--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -1,14 +1,11 @@
 import { initRatings } from './rating-modal';
-import { getFromLocalStorage } from './favorites-recipes';
+import { getDataFromLocalStorage } from './local-storage-api';
 
 export function createCards(cards, container) {
     if (!cards.length) {
-
         container.innerHTML =
             '<p>Nothing was found for your request. Try changing your search parameters...</p>';
         return;
-        
-
     }
 
     container.innerHTML = cards
@@ -38,9 +35,11 @@ export function createCards(cards, container) {
 
     initRatings();
 
+    console.log(getDataFromLocalStorage());
+
     document.querySelectorAll('.icon-heart').forEach(icon => {
         if (icon) {
-            getFromLocalStorage()
+            getDataFromLocalStorage()
                 .map(favorite => favorite._id)
                 .forEach(favId => {
                     if (favId === icon.dataset.id) {
