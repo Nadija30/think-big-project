969d0d2eab4918715ad2115160db36fe51a89dd3
Tatiana <tatka_co@ukr.net>
2023-08-24 01:14:18 +0300

    new

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -9,3 +9,18 @@ mobMenuOpenBtn.addEventListener('click', function() {
 mobMenuCloseBtn.addEventListener('click', function() {
     mobileMenu.classList.add('is-hidden');
 });
+
+
+
+const headerLinkHome = document.querySelector('.header-link-home');
+const headerLinkFavorites = document.querySelector('.header-link-favorites');
+
+headerLinkHome.addEventListener('click', function() {
+    headerLinkHome.classList.add('active');
+    headerLinkHome.classList.remove('active');
+});
+
+headerLinkFavorites.addEventListener('click', function () {
+    headerLinkFavorites.classList.add('active');
+    headerLinkFavorites.classList.remove('active');
+});
