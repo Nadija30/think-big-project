8ce8b757b82a316fa3acba213707f3e79a5fa1f4
Tatiana <tatka_co@ukr.net>
2023-08-24 06:30:57 +0300

    header-u

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -10,17 +10,29 @@ mobMenuCloseBtn.addEventListener('click', function() {
     mobileMenu.classList.add('is-hidden');
 });
 
+// "active"
 
+document.addEventListener("DOMContentLoaded", function() {
+        const headerLinkHome = document.getElementById("home-link");
+        const headerLinkFavorites = document.getElementById("favorites-link");
 
-const headerLinkHome = document.querySelector('.header-link-home');
-const headerLinkFavorites = document.querySelector('.header-link-favorites');
+        function setActiveLink(link) {
+            headerLinkHome.classList.remove("active");
+            headerLinkFavorites.classList.remove("active");
+            link.classList.add("active");
+        }
 
-headerLinkHome.addEventListener('click', function() {
-    headerLinkHome.classList.add('active');
-    headerLinkHome.classList.remove('active');
-});
+        headerLinkHome.addEventListener("click", function(event) {
+            setActiveLink( headerLinkHome);
+        });
 
-headerLinkFavorites.addEventListener('click', function () {
-    headerLinkFavorites.classList.add('active');
-    headerLinkFavorites.classList.remove('active');
+        headerLinkFavorites.addEventListener("click", function(event) {
+            setActiveLink(headerLinkFavorites);
+        });
+
+        if (window.location.pathname.includes("Favorites.html")) {
+            setActiveLink(headerLinkFavorites);
+        } else {
+            setActiveLink(headerLinkHome);
+        }
 });
