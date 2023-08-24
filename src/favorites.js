7454c5451ddb8ccc65d550c864492a5b02b13b1c
Vlad <00cmb.ua@gmail.com>
2023-08-24 21:09:56 +0300

    includes in favorites.html

--- src/favorites.js
+++ src/favorites.js
@@ -3,3 +3,6 @@ import './js/favorites-recipes';
 import './js/footer';
 import './js/render-cards';
 import './js/change-theme';
+import './js/order.modal';
+import './js/pop-up-modal';
+import './js/rating-modal';
