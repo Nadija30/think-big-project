c6552a41f11067178662bb490eeeb727dc02477b
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-08-24 16:09:04 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './js/swiper';
 import './js/burger-menu';
-import './js/categories';
-import './js/filters';
 import './js/pop-up-modal';
 import './js/popular-recipes';
 import './js/recipes';
