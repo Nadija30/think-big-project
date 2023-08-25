893ae63ba71f85dc37b92871ca7a97166e60c2d4
SaltyUA <00cmb.ua@gmail.com>
2023-08-25 12:14:35 +0300

    fix all

--- src/js/favorites-recipes.js
+++ src/js/favorites-recipes.js
@@ -114,17 +114,29 @@ function createFavCards(cards) {
     favorGallBox.innerHTML = '';
     cards.forEach(card => {
         const favoriteCard = `<li class="favorites__gallery-list-item">
-                   <div class="favorites__card">
-                   <img src="${card.preview}" alt="${card.title}" class="favorites__card-image">
+                   <div class="favorites__card" style="background: var(--light-theme-background-color-cards-gradient),
+        url(${card.preview});
+    background-repeat: no-repeat;
+    background-size: cover;">
                     <div class="favorites__card-info">
                      <h2 class="favorites__card-tittle">${card.title}</h2>
-                      <p class="favorites__card-description">${card.description}</p>
-                      <div class="rating card__rating"><p class="rating__value">${card.rating}</p></div>
-                      <button class="favorites__card-btn">See recipe</button>
+                      <p class="favorites__card-description">${
+                          card.description
+                      }</p>
+                      <div class="rating card__rating"><p class="rating__value">${card.rating.toFixed(
+                          1
+                      )}</p></div>
+                      <button class="favorites__card-btn js-see-recipe" data-id="${
+                          card._id
+                      }">See recipe</button>
                    </div>
                    <button class="favorites__card-heart" type="button">
-               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" class="favorites__svg-heart" data-id="${card._id}">
-               <path class="favorites__svg-path" data-id="${card._id}" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" class="favorites__svg-heart" data-id="${
+                   card._id
+               }">
+               <path class="favorites__svg-path" data-id="${
+                   card._id
+               }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                    d="M10.994 4.708C9.162 2.565 6.105 1.988 3.81 3.95c-2.296 1.962-2.62 5.242-.816 7.563 1.5 1.929 6.037 5.998 7.524 7.315.166.147.25.221.346.25a.464.464 0 0 0 .262 0c.097-.029.18-.103.347-.25 1.487-1.317 6.025-5.386 7.524-7.315 1.803-2.32 1.52-5.622-.816-7.563-2.336-1.942-5.353-1.386-7.186.757Z"
                    clip-rule="evenodd" />
                </svg>
